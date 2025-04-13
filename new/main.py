8f76504aa132444d1b40058ccfc8be1edf9e2746
kuka1234 <shreenidhikandekar@gmail.com>
2025-04-13 19:43:22 +0100

    Added training, testing and logging code for base and pruned models

--- new/main.py
+++ new/main.py
@@ -1,7 +1,15 @@
 import os
 import argparse
+import random
 import torch
 from torchvision import datasets, transforms
+import shutil
+import vgg
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import wandb
 
 # Default cifar-10 from old code.
 def get_cifar10(train_batch_size, test_batch_size, kwargs):
@@ -35,6 +43,7 @@ def get_cifar10(train_batch_size, test_batch_size, kwargs):
 def main():
     # Training Settings
     parser = argparse.ArgumentParser(description='PyTorch Slimming CIFAR training')
+    parser.add_argument('run_name', type=str, help='Name of the log run', required=True)
     parser.add_argument('--sparsity-regularization', '-sr', dest='sr', action='store_true',
                         help='train with channel sparsity regularization')
     parser.add_argument('--s', type=float, default=0.0001,
@@ -53,11 +62,13 @@ def main():
                         metavar='W', help='weight decay (default: 1e-4)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='disables CUDA training')
+    parser.add_argument('--fine-tuning',default='', type=str, metavar='PATH',
+                        help='path to the pruned model to be fine tuned')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                         help='how many batches to wait before logging training status')
-    parser.add_argument('--save', default='./logs', type=str, metavar='PATH',
+    parser.add_argument('--save', type=str, metavar='PATH',
                         help='path to save prune model (default: current directory)')
     parser.add_argument('--depth', default=16, type=int,
                         help='depth of the neural network')
@@ -75,7 +86,126 @@ def main():
     kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
     train_loader, test_loader = get_cifar10(args.batch_size, args.test_batch_size, kwargs)
 
-    # To add training and testing here
+    if args.fine_tuning:
+        checkpoint = torch.load(args.fine_tuning)
+        model = vgg(dataset=args.dataset, depth=args.depth, cfg=checkpoint['cfg'])
+        model.load_state_dict(checkpoint['state_dict'])
+    else:
+        model = vgg(dataset=args.dataset, depth=args.depth)
+
+    if args.cuda:
+        model.cuda()
+
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            best_prec1 = checkpoint['best_prec1']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {}) Prec1: {:f}"
+                .format(args.resume, checkpoint['epoch'], best_prec1))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
+
+    def updateBN():
+        for m in model.modules():
+            if isinstance(m, nn.BatchNorm2d):
+                m.weight.grad.data.add_(args.s*torch.sign(m.weight.data))  # L1
+
+    def train(epoch):
+        model.train()
+        avg_loss = 0.
+        train_acc = 0.
+        for batch_idx, (data, target) in enumerate(train_loader):
+            if args.cuda:
+                data, target = data.cuda(), target.cuda()
+            data, target = Variable(data), Variable(target)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = F.cross_entropy(output, target)
+            avg_loss += loss.data[0]
+            pred = output.data.max(1, keepdim=True)[1]
+            train_acc += pred.eq(target.data.view_as(pred)).cpu().sum()
+            loss.backward()
+            if args.sr:
+                updateBN()
+            optimizer.step()
+            if batch_idx % args.log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.1f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), loss.data[0]))
+
+        wandb.log({
+            "training_loss": avg_loss,
+            "training_accuracy": train_acc / len(train_loader.dataset),
+            "epoch": epoch,
+            "lr": optimizer.param_groups[-1]['lr']
+            }
+        , step=epoch)
+
+    def test():
+        model.eval()
+        test_loss = 0
+        correct = 0
+        for data, target in test_loader:
+            if args.cuda:
+                data, target = data.cuda(), target.cuda()
+            data, target = Variable(data, volatile=True), Variable(target)
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, size_average=False).data[0] # sum up batch loss
+            pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+        test_loss /= len(test_loader.dataset)
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.1f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
+        
+        wandb.log({
+            "validation_loss": test_loss,
+            "validation_accuracy": correct / len(test_loader.dataset),
+            "epoch": epoch,
+            "lr": optimizer.param_groups[-1]['lr']
+            }
+        , step=epoch)
+
+        return correct / float(len(test_loader.dataset))
+
+    def save_checkpoint(state, is_best, filepath):
+        torch.save(state, os.path.join(filepath, 'checkpoint.pth.tar'))
+        if is_best:
+            shutil.copyfile(os.path.join(filepath, 'checkpoint.pth.tar'), os.path.join(filepath, 'model_best.pth.tar'))
+
+    args.run_name = args.run_name + random.getrandbits(16)  # Add random bits to the run name to avoid overwriting
+    with wandb.init(
+        project="network_pruning", 
+        name=args.run_name, 
+        config={"epochs": args.epochs, "lr": args.lr, "fine tuning": True if args.fine_tuning != "" else False} + {"sr": args.s} if args.sr else {},
+        tags=["vgg", "pruning"],
+        mode="offline",
+        dir="./wandb_logs"
+    ):
+        wandb.watch(model, log="all", log_freq = 100)
+        best_prec1 = 0.
+        for epoch in range(args.start_epoch, args.epochs):
+            if epoch in [args.epochs*0.5, args.epochs*0.75]:
+                for param_group in optimizer.param_groups:
+                    param_group['lr'] *= 0.1
+            train(epoch)
+            prec1 = test()
+            is_best = prec1 > best_prec1
+            best_prec1 = max(prec1, best_prec1)
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'state_dict': model.state_dict(),
+                'best_prec1': best_prec1,
+                'optimizer': optimizer.state_dict(),
+                'cfg': model.cfg
+            }, is_best, filepath=args.run_name)
 
 if __name__=='__main__':
     main()
